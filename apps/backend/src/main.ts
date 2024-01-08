d801afc04e0953811241224de0133ab95e6d8f4b
Henrry Bourgeot <henrrybrgt@gmail.com>
2024-01-08 08:13:26 -0400

    fixing

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,26 +1,30 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { NextFunction } from 'express';
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
 
 async function importHandler() {
-  const modulePath = '../../frontend/build/handler.js';
-  const module = await import(modulePath);
-  return module.handler;
+  return new Function(`return import('../../frontend/build/handler.js')`)();
 }
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('api');
   let port = 3000;
   if (process.env.NODE_ENV === 'production') {
     port = 8080;
-    const handler = await importHandler();
 
-    app.use((req: Request, res: Response, next: NextFunction) => {
+    const handler = (await importHandler()).handler;
+
+    app.use((req: Request, res: Response, next: any) => {
       if (req.url.startsWith('/api')) {
         return next();
       }
