ffc075c72c0d3cc0c54c6ac951aeb6b8fd982dac
Henrry Bourgeot <henrrybrgt@gmail.com>
2024-01-07 23:29:23 -0400

    deleting app controller

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
 import { LinksModule } from './links/links.module';
 
 @Module({
   imports: [PostsModule, CommentsModule, LinksModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
